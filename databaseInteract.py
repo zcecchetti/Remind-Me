3326de87058632c28efad15ce2f67fd6e3bcff2c
Zachary Cecchetti <zcecchetti@gmail.com>
2024-06-20 23:07:36 -0700

    Added Function that returns all alerts that must be sent today in a list.

--- databaseInteract.py
+++ databaseInteract.py
@@ -1,6 +1,7 @@
 # use this script for functions to interact with SQLite Alerts database
 
 import sqlite3
+from datetime import date
 
 con = sqlite3.connect("/Users/zach/Documents/Python/remindMe/Alerts.db")
 
@@ -12,10 +13,10 @@ def countAlerts ():
 
     return(alert_count)
 
-def addNewAlert (alert_name, alert_details, alert_recipients, alert_date, recurring):
+def addNewAlert (alert_name, alert_details, alert_recipients, alert_date, alert_time, recurring):
     new_alert_id = countAlerts() + 1 
-    new_data = [new_alert_id, alert_name, alert_details, alert_recipients, alert_date, recurring]
-    cur.execute("INSERT INTO Alerts VALUES ( ?, ?, ?, ?, ?, ?)", new_data)
+    new_data = [new_alert_id, alert_name, alert_details, alert_recipients, alert_date, alert_time, recurring]
+    cur.execute("INSERT INTO Alerts VALUES ( ?, ?, ?, ?, ?, ?, ?)", new_data)
     con.commit()
 
     return new_data
@@ -34,11 +35,20 @@ def readAllAlerts ():
     
     return alertList
 
+def checkDailyAlerts ():
+    alertList = []
+    today = date.today()
+    for row in cur.execute("select alert_date, alert_name, alert_details, alert_time FROM Alerts WHERE alert_date = ?", (today, )):
+        alertList.append(row)
 
+    print(today)
+
+    return alertList
 
 print(countAlerts())
-print(addNewAlert("Roux Birthday", "Today is Rouxs birthday", "Everyone", "2005-11-22 00:00:00.000", "1" ))
+print(addNewAlert("Roux Birthday", "Today is Rouxs birthday", "Everyone", "2005-11-22", "12:00:00.000", "1" ))
 print(readAllAlerts())
 removeAlert()
 print(readAllAlerts())
+print(checkDailyAlerts())
 
